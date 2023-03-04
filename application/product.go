a41f8dd43ceb40a5eb379b9a94a81e1a15131529
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-03-04 17:05:06 -0300

    Creating product disable test

--- application/product.go
+++ application/product.go
@@ -37,7 +37,11 @@ func (p *Product) Enable() error {
 }
 
 func (p *Product) Disable() error {
-	return nil
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
+	}
+	return errors.New("price must be zero to disable the product")
 }
 
 func (p *Product) GetId() string {
--- application/product_test.go
+++ application/product_test.go
@@ -18,3 +18,17 @@ func TestProduct_Enable(t *testing.T) {
 	err = product.Enable()
 	require.Equal(t, "price must be greater than zero to enable the product", err.Error())
 }
+
+func TestProduct_Disable(t *testing.T) {
+	product := Product{}
+	product.Name = "Smartphone"
+	product.Status = ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "price must be zero to disable the product", err.Error())
+}
